bfbf2ff88843de0aa13135f9566bde7a28424b92
CaboML <38300412+CaboML@users.noreply.github.com>
2018-07-31 17:47:03 +0100

    Intermediate exercices completed

--- SQL with PostgreSQL.sql
+++ SQL with PostgreSQL.sql
@@ -168,3 +168,393 @@ GROUP BY
   contactTitle
 ORDER BY
   COUNT(contactTitle) desc ;
+
+----- Problem 18 ---- JOIN TWO TABLES
+
+SELECT productID, productName, companyName FROM products
+JOIN suppliers
+  ON suppliers.supplierID = products.supplierID;
+
+----- Problem 19 ----
+
+SELECT
+  orderID, date(orderDate) as orderdate,  CompanyName as supplier FROM orders
+JOIN shippers
+  ON shippers.ShipperID = orders.shipVia
+WHERE orderID < 10300
+order by orderID ASC ;
+---------------------------------------------------------------------
+----------------------- Intermediate problems------------------------
+---------------------------------------------------------------------
+
+------- Problem 20 --------------------
+SELECT CategoryName, count(*) as total_products FROM Categories
+
+JOIN products
+  ON Categories.CategoriesID = products.categoryID
+
+GROUP BY CategoryName
+
+ORDER BY total_products desc
+;
+
+-------- Problem 21 -------
+
+SELECT * FROM customers;
+
+SELECT  country ,city ,count(*)  as TotalCustomer FROM customers
+group by city, country
+ORDER BY TotalCustomer desc
+;
+
+------- Problem 22 --------
+SELECT * FROM products;
+
+SELECT
+  productID,
+  unitsInStock,
+  reorderLevel
+FROM
+  products
+WHERE
+  unitsInStock < reorderLevel;
+
+----------Problem 23 --------
+SELECT
+  productID,
+  productName,
+  unitsInStock,
+  unitsOnOrder,
+  reorderLevel,
+  discontinued
+FROM
+  products
+WHERE
+  unitsInStock + unitsOnOrder <= reorderLevel
+AND
+  discontinued = 0;
+
+------------ Problem 24 ---------
+
+SELECT
+  customerID,
+  companyName,
+  region
+FROM customers
+order by
+  Case
+ when Region is null then 1
+ else 0
+END,
+  region ASC,
+customerID asc
+;
+
+------- Problem 25 ------ How to show top 3
+SELECT  * FROM orders;
+
+SELECT
+  shipCountry,
+  AVG(freight) AS AverageFreight
+FROM orders
+GROUP BY shipCountry
+ORDER BY AverageFreight DESC
+limit 3;
+
+------- Problem 26 --------
+SELECT
+  shipCountry,
+  AVG(freight) AS AverageFreight
+FROM orders
+  WHERE date_part('year',orderDate) > 1996 AND date_part('year',orderDate)<1998
+GROUP BY shipCountry
+ORDER BY AverageFreight DESC
+limit 3;
+
+-------- Problem 27 -------
+select * from orders order by OrderDate;
+
+-- The between statement is exclusive, which means that the last values on 31 December is not included
+
+------- Problem 28-------
+
+SELECT
+  shipCountry,
+  AVG(freight) AS AverageFreight
+FROM orders
+  WHERE
+  (OrderDate) > (SELECT Max((OrderDate)) from Orders) - interval '12 month'
+
+Group by ShipCountry
+ORDER BY AverageFreight DESC
+LIMIT 3
+;
+
+--------- Problem 29 -------------
+-- orders join employee -->> employee lastname and order join order details -->> product Id and quantity finally order details JOIN products -->> product name
+
+SELECT
+  O.employeeID ,
+  E.lastname,
+  O.orderID,
+  P.productName,
+  OD.quantity
+FROM
+  orders as O
+JOIN employee as E
+  ON O.employeeID = E.employeeID
+JOIN order_details AS OD
+    ON OD.orderID = O.orderID
+JOIN products AS P
+  ON P.productID = OD.productID
+
+ORDER BY O.orderID, P.productID;
+
+------- Problem 30 ------------ Left Join
+
+Select
+ Customers.CustomerID as Customers_CustomerID,
+  Orders.CustomerID as Orders_CustomerID
+ From Customers
+LEFT JOIN  Orders
+ on Orders.CustomerID = Customers.CustomerID
+WHERE orders.customerID IS NULL
+;
+
+--------- PROBLEM 31 ------------- Sub queries.
+
+--Subqueries on select statment must return single values
+
+Select
+ Customers.CustomerID as Customers_CustomerID,
+  Orders.CustomerID as Orders_CustomerID
+ From Customers
+LEFT JOIN  (SELECT customerID
+     from orders
+     where
+     employeeID = 4) AS Orders
+ on Orders.CustomerID = Customers.CustomerID
+WHERE orders.customerID IS NULL
+;
+
+----Alternatives
+-- The most common way to solve this kind of problem is as above, with a left join. However, here are some alternatives using Not In and Not Exists.
+--  Select CustomerID
+--  From Customers
+--  Where
+--  CustomerID not in (select CustomerID from Orders where EmployeeID = 4)
+--  Select CustomerID
+--  From Customers
+--  Where Not Exists
+--  (
+--  Select CustomerID
+--  from Orders
+--  where Orders.CustomerID = Customers.CustomerID
+--  and EmployeeID = 4
+--  )
+
+---------------------------------------------------------------------
+----------------------- Advanced problems------------------------
+---------------------------------------------------------------------
+
+-------- Problem 32----------
+SELECT
+  customers.customerID,
+  customers.companyName,
+  orders.orderID,
+  sum(unitPrice * quantity) as total_value
+  from customers
+
+ join orders
+
+    ON orders.customerID = orders.customerID
+
+join order_details
+
+    ON orders.orderID = order_details.orderID
+
+WHERE  OrderDate >= '19980101'
+ and OrderDate  < '19990101'
+
+GROUP BY
+  orders.orderID, customers.customerID
+
+having sum(unitPrice * quantity) > 10000
+
+ORDER BY total_value DESC
+;
+
+-------- Problem 33 -------------------------- more than 15k in 2016
+
+SELECT
+  customers.customerID,
+  customers.companyName,
+  sum(unitPrice * quantity) as total_value
+  from customers
+
+ join orders
+
+    ON orders.customerID = orders.customerID
+
+join order_details
+
+    ON orders.orderID = order_details.orderID
+
+WHERE  OrderDate >= '19980101'
+ and OrderDate  < '19990101'
+
+GROUP BY
+   customers.customerID, companyName
+
+having sum(unitPrice * quantity) > 15000
+
+ORDER BY total_value DESC
+;
+
+----- Problem 34 ---------------------------------- same problem as preview with discount
+SELECT
+  customers.customerID,
+  customers.companyName,
+  sum(unitPrice * quantity) as total_value,
+  sum(unitPrice * quantity * (1 - discount)) as total_value_discount
+
+  from customers
+
+ join orders
+
+    ON orders.customerID = orders.customerID
+
+join order_details
+
+    ON orders.orderID = order_details.orderID
+
+WHERE  OrderDate >= '19980101'
+ and OrderDate  < '19990101'
+
+GROUP BY
+   customers.customerID, companyName
+
+having sum(unitPrice * quantity * (1 - discount)) > 15000
+
+ORDER BY total_value_discount DESC
+;
+
+------------ Problem 35 ----------------------------------------------
+SELECT
+  employeeID,
+  orderID,
+  orderDate,
+   date_trunc('month', Date(orderDate)) + interval '1 month' - interval '1 day' AS DATEsss
+
+  FROM orders
+
+WHERE orderDate = date_trunc('month', Date(orderDate)) + interval '1 month' - interval '1 day'
+
+ORDER BY employeeID
+
+--------- Problem 36 --------------------------- Count()
+SELECT
+  orders.orderID,
+  count(*) as TotalNumber
+FROM
+  order_details
+  join orders
+  on orders.orderID = order_details.orderID
+
+group by orders.orderID
+order by TotalNumber DESC
+limit 10;
+
+--------- Problem 37 --------------------------- Random
+
+SELECT orderID
+FROM
+  orders
+WHERE
+  random() < 0.01;
+
+---------- Problem 38 -------------
+
+SELECT
+  order_details.orderID,
+  order_details.quantity,
+  count(order_details.quantity) as Duplicate
+
+
+FROM order_details
+
+--   join orders
+--   on orders.orderID = order_details.orderID
+--
+--   join employee
+--   on employee.lastName = 'Leverling'
+
+WHERE quantity >= 60
+
+
+
+group by order_details.orderID,  quantity
+having count(*) >1
+
+order by order_details.orderID ASC
+
+;
+--------- Problem 39 --------------- USING "with" tables
+-- Oldest employee
+with PotentianDuplicates as (
+    SELECT orderID
+    FROM
+      order_details
+    WHERE quantity >= 60
+    GROUP BY orderID, quantity
+    HAVING COUNT(*) > 1
+)
+SELECT
+  order_details.orderID,
+  productID,
+  unitPrice,
+  quantity,
+  discount
+  FROM
+    order_details
+
+WHERE
+  orderID in (SELECT orderID FROM PotentianDuplicates)
+ORDER BY
+  orderID,
+  quantity
+;
+
+----------- problem 40 ----- Derived table
+
+SELECT
+  order_details.orderID,
+  productID,
+  unitPrice,
+  qUANTITY,
+  discount
+FROM order_details
+
+  join (SELECT DISTINCT
+      orderID
+    FROM order_details
+    WHERE quantity >=60
+    GROUP BY orderID, quantity
+    HAVING COUNT(*)> 1
+    ) as POTENTIALPROBLEMORDERS
+  ON POTENTIALPROBLEMORDERS.orderID = order_details.orderID
+ORDER BY  order_details.orderID, productID
+
+------- Problem 41 ----------------------------------------- Late orders
+SELECT
+  orderID,
+  orderDate,
+  requiredDate,
+  shippedDate
+FROM
+  orders
+where
+  shippedDate > requiredDate
+;
+
+
